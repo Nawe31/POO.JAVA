229bf9ffd598b04da89ab1464cad1ed75489d274
nahuel francia <nahuel_francia95@hotmail.com>
2023-05-08 23:38:15 -0300

    agregando nuevos ejercicios

--- POO8/src/Clases/Cadena.java
+++ POO8/src/Clases/Cadena.java
@@ -11,6 +11,8 @@ frase ingresada.
  */
 package Clases;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nahue
@@ -18,69 +20,178 @@ package Clases;
 public class Cadena {
   
     
-   String frace;
-   int logitud = frace.length();
+   String frase;
 
     public Cadena() {
     }
 
-    public Cadena(String frace) {
-        this.frace = frace;
+    public Cadena(String frase) {
+        this.frase = frase;
     }
 
-    public String getFrace() {
-        return frace;
+    public String getFrase() {
+        return frase;
     }
 
-    public void setFrace(String frace) {
-        this.frace = frace;
-    }
-
-    public int getLogitud() {
-        return logitud;
-    }
-
-    public void setLogitud(int logitud) {
-        this.logitud = logitud;
+    public void setFrase(String frase) {
+        this.frase = frase;
     }
 
     @Override
     public String toString() {
-        return "Cadena{" + "frace=" + frace + ", logitud=" + logitud + '}';
+        return "Cadena{" + "frase=" + frase + '}';
     }
    
-   public void mostrarVocales(int longitud, String frace){
-       for (int i = 0; i < frace.length() ; i++) {
-          int vocales = 0;
+
+
+   
+   public void mostrarVocales( String frase){
+       
+       int vocales = 0;
+       for (int i = 0; i < frase.length() ; i++) {
+         
           
-          if ( frace.charAt(i)=='a'|| frace.charAt(i)=='e'|| frace.charAt(i)=='i'|| frace.charAt(i)=='o'|| frace.charAt(i)=='u' ){
+        if (frase.charAt(i)=='a' || frase.charAt(i)=='e'|| frase.charAt(i)=='i'|| frase.charAt(i)=='o'|| frase.charAt(i)=='u' ){
               
               
               vocales++;
           }
            
-           System.out.println(" LA CANTIDAD DE VOCALES ES "+ vocales);
+         
        }
+       System.out.println(" LA CANTIDAD DE VOCALES ES  " + vocales);
    
    }
    
    /*Método invertirFrase(), deberá invertir la frase ingresada y mostrarla por pantalla. Por
 ejemplo: Entrada: "casa blanca", Salida: "acnalb asac".*/
    
-   public void invertirFrase(int longitud, String frace){
+   public void invertirFrase( String frase){
    
     
            
            
-            for (int i = frace.length()-1; i > 0; i--) {
+            for (int i = frase.length()-1; i > 0; i--) {
             
             
-            System.out.println(frace.charAt(i));
+            System.out.println(frase.charAt(i));
         }
        
        
    }
     
     
+  /*vecesRepetido(String letra), recibirá un carácter ingresado por el usuario y
+contabilizar cuántas veces se repite el carácter en la frase, por ejemplo:
+Entrada: frase = "casa blanca". Salida: El carácter 'a' se repite 4 veces.*/
+   
+   public void vecesRepetido(String frase){
+       Scanner leer = new Scanner(System.in);
+       String letra = "";
+       System.out.println(" ingrese la letra que quiere buscar");
+       letra = leer.next();
+       int z = 0;
+       for (int i = 0; i < frase.length(); i++) {
+     
+           
+           if(frase.substring(i,i+1).equals(letra)){
+           
+               z++;
+               
+           }
+         
+       }
+         System.out.println(" su letra esta "+ (z)+ " veces en la frace" );
+       
+   }
+   
+   
+   /* metodo compararLongitud(string frace), debecomparar la longitud de la frace
+   que compone la clase con otra frace nueva ingresada por el usuario
+   */
+   
+   public void compararLongitud(String frase){
+       
+      Scanner leer = new Scanner(System.in);
+      String fraseN; 
+      
+       System.out.println(" ingrese una nueva frace ");
+       fraseN = leer.nextLine();
+       
+       if (frase.length() >= fraseN.length()){
+       
+           System.out.println(" la frace inicual es mayo o igual que la segunda frace ingresada ");
+       }
+       else{
+           System.out.println(" la primer frace ingresada es menor que la segunda ");
+       }
+   
+   }
+   
     
+/* Método unirFrases(String frase), deberá unir la frase contenida en la clase Cadena
+con una nueva frase ingresada por el usuario y mostrar la frase resultante.*/
+
+public void unirFrases(String frase){
+
+ Scanner leer = new Scanner(System.in);
+ String fraseU, resultadoU; 
+      
+ System.out.println(" ingrese una nueva frace ");
+ fraseU = leer.nextLine();
+       
+       resultadoU = (frase + fraseU);
+       
+       System.out.println(" la frase unida es "+resultadoU);
 }
+
+/* Método reemplazar(String letra), deberá reemplazar todas las letras “a” que se
+encuentren en la frase, por algún otro carácter seleccionado por el usuario y mostrar
+la frase resultante.*/
+
+public void reemplazar(String frase){
+
+  
+        
+        String remplazar,fraseR;
+        Scanner leer = new Scanner(System.in);
+        System.out.println(" ingrese la letra que se reemplazara por la A");
+        remplazar = leer.nextLine();
+        
+
+        
+        fraseR = frase.replace("a",remplazar);
+             
+         System.out.println( fraseR);
+}
+    
+   
+/* Método contiene(String letra), deberá comprobar si la frase contiene una letra que
+ingresa el usuario y devuelve verdadero si la contiene y falso si no. */
+
+public void contiene(String frase){
+    String contiene;
+    boolean auxC;
+       auxC = false;
+    Scanner leer = new Scanner(System.in);
+    System.out.println(" ingrese una letra para comprobar si esta en la frace");
+    contiene = leer.nextLine();
+    
+    
+    for (int i = 0; i < frase.length(); i++) {
+        
+        if (frase.substring(i,i+1).equals(contiene) ){
+            auxC = true;
+            }
+       
+        
+        }
+    System.out.println(" "+auxC+" " );
+}
+
+
+}
+
+
+
+
--- POO8/src/poo8/POO8.java
+++ POO8/src/poo8/POO8.java
@@ -11,6 +11,7 @@ package poo8;
 import Clases.Cadena;
 import java.util.Scanner;
 
+
 /**
  *
  * @author nahue
@@ -21,25 +22,55 @@ public class POO8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       /* Scanner leer = new Scanner(System.in);
-       Cadena c1 = new Cadena();
+         Scanner read = new Scanner(System.in);
+       
         
-        System.out.println("Ingrese una frace o varias palabras separadas");
-        c1.setFrace(leer.next());*/
+        Cadena op = new Cadena();
         
-          
-        Scanner leer = new Scanner(System.in);
+        System.out.println(" ingrese una frase");
+        op.setFrase(read.next());
         
-        
-        String frace;
-        System.out.println("Ingrese una frace o varias palabras separadas");
-        frace = leer.next();
+           System.out.println(" elija la operacion que desee  ");
+        System.out.println("  1 --- PARA MOSTRAR VOCALES----");
+        System.out.println("  2 --- PARA INVERTIR FRASE----");
+        System.out.println("  3 ---  COMPARAR LONGITUD---");
+        System.out.println("  4 --- UNIR FRASE---");
+        System.out.println("  5 --- REEMPLAZAR LETRA---");
+         System.out.println(" 6 --- COMPROBAR SI CONTIENE UNA LETRA---");
     
-        for (int i = frace.length()-1; i > 0; i--) {
-            
-            
-            System.out.println(frace.charAt(i));
-        }
-    }
+        
+        
+         int num = read.nextInt();
+         
+         
+         switch(num){
+             case 1:{
+              op.mostrarVocales(op.getFrase());
+                 
+             }
+             case 2:{
+             op.invertirFrase(op.getFrase());
+             break;
+             }
+             case 3:{
+             op.compararLongitud(op.getFrase());
+             break;
+             }
+              case 4:{
+             op.unirFrases(op.getFrase());
+             break;
+             }
+             case 5:{
+             op.reemplazar(op.getFrase());
+             break;
+             }
+             case 6:{
+             op.contiene(op.getFrase());
+             break;
+             }
+         
+         
+         
+ }
     
-}
+}}
